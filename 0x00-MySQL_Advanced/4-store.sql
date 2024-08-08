1dab6d4223c8a13b4bc15d2fef794fcf69738fe7
fevi29 <fevenbayu5@gmail.com>
2024-08-08 23:11:18 +0300

    advanced

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- creates a trigger that decreses the quanity of an item
+-- afyter adding a new order
+DELIMITER $$ 
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON `orders`
+FOR EACH ROW
+BEGIN
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
